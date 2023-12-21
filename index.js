db4f83176084e12875a73ffefb3e0b4cd1b6b1cf
monsieurluge <monsieurluge@gmail.com>
2023-12-21 11:38:27 +0100

    refacto: resolver call

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import day1 from './puzzles/day1/day1.js'
 
 const resolvers = new Map([
-    ['day1', day1.resolve],
+    ['day1', day1],
 ])
 
 const requestedResolver = process.argv[2]
@@ -10,8 +10,6 @@ if (!resolvers.has(requestedResolver)) {
     throw new Error(`unknown resolver name: "${requestedResolver}"`)
 }
 
-const resolver = resolvers.get(requestedResolver)
-
-const response = resolver(...process.argv.slice(3))
+const response = resolvers.get(requestedResolver).resolve(...process.argv.slice(3))
 
 console.log(response)
