8a711f72cbe0f6c5adaa283b4046b29bdb1433ce
monsieurluge <monsieurluge@gmail.com>
2023-12-22 10:35:16 +0100

    day1: part two, updating the tests and processes

--- src/puzzles/day1/day1.js
+++ src/puzzles/day1/day1.js
@@ -1,22 +1,52 @@
 import { lines } from '../../common/file.js'
 
-const digitsRegExp = /\D*(\d)/
+const firstDigitRegExp = /(\d|one|two|three|four|five|six|seven|eight|nine).*/
+
+const lastDigitRegExp = /.*(\d|one|two|three|four|five|six|seven|eight|nine)/d
+
+const validDigits = new Map([
+    ['1', 1],
+    ['2', 2],
+    ['3', 3],
+    ['4', 4],
+    ['5', 5],
+    ['6', 6],
+    ['7', 7],
+    ['8', 8],
+    ['9', 9],
+    ['one', 1],
+    ['two', 2],
+    ['three', 3],
+    ['four', 4],
+    ['five', 5],
+    ['six', 6],
+    ['seven', 7],
+    ['eight', 8],
+    ['nine', 9],
+])
 
 const firstDigit = (word) => {
-    const matches = digitsRegExp.exec(word)
+    const matches = firstDigitRegExp.exec(word)
     if (!Array.isArray(matches) || matches.length < 2) {
         throw new Error(`no digit found in "${word}"`)
     }
-    const digit = Number(matches[1])
-    if (Number.isNaN(digit)) {
+    const digit = matches[1]
+    if (!validDigits.has(digit)) {
         throw new Error(`the first result is not a number, found: ${digit}`)
     }
-    return digit
+    return validDigits.get(digit)
 }
 
 const lastDigit = (word) => {
-    const reversedWord = word.split('').reverse().join('')
-    return firstDigit(reversedWord)
+    const matches = lastDigitRegExp.exec(word)
+    if (!Array.isArray(matches) || matches.length < 2) {
+        throw new Error(`no digit found in "${word}"`)
+    }
+    const digit = matches[1]
+    if (!validDigits.has(digit)) {
+        throw new Error(`the first result is not a number, found: ${digit}`)
+    }
+    return validDigits.get(digit)
 }
 
 export const lineCalibration = (word) => firstDigit(word) * 10 + lastDigit(word)
--- src/puzzles/day1/day1.test.js
+++ src/puzzles/day1/day1.test.js
@@ -3,20 +3,29 @@ import { calibration, lineCalibration } from './day1.js'
 
 test('line calibration function should join the first and last digits encountered in a string', (expect) => {
     // arrange
-    const string1 = '1abc2'
-    const string2 = 'pqr3stu8vwx'
-    const string3 = 'a1b2c3d4e5f'
-    const string4 = 'treb7uchet'
+    const string1 = 'two1nine'
+    const string2 = 'eightwothree'
+    const string3 = 'abcone2threexyz'
+    const string4 = 'xtwone3four'
+    const string5 = '4nineeightseven2'
+    const string6 = 'zoneight234'
+    const string7 = '7pqrstsixteen'
     // act
     const result1 = lineCalibration(string1)
     const result2 = lineCalibration(string2)
     const result3 = lineCalibration(string3)
     const result4 = lineCalibration(string4)
+    const result5 = lineCalibration(string5)
+    const result6 = lineCalibration(string6)
+    const result7 = lineCalibration(string7)
     // assert
-    expect.equal(result1, 12)
-    expect.equal(result2, 38)
-    expect.equal(result3, 15)
-    expect.equal(result4, 77)
+    expect.equal(result1, 29)
+    expect.equal(result2, 83)
+    expect.equal(result3, 13)
+    expect.equal(result4, 24)
+    expect.equal(result5, 42)
+    expect.equal(result6, 14)
+    expect.equal(result7, 76)
     expect.end()
 })
 
@@ -32,14 +41,17 @@ test('line calibration function should crash if the line is empty', (expect) => 
 test('calibration function should apply the line calibration on each line and add the values', (expect) => {
     // arrange
     const lines = [
-        '1abc2',
-        'pqr3stu8vwx',
-        'a1b2c3d4e5f',
-        'treb7uchet',
+        'two1nine',
+        'eightwothree',
+        'abcone2threexyz',
+        'xtwone3four',
+        '4nineeightseven2',
+        'zoneight234',
+        '7pqrstsixteen',
     ]
     // act
     const result = calibration(lines)
     // assert
-    expect.equal(result, 142)
+    expect.equal(result, 281)
     expect.end()
 })
